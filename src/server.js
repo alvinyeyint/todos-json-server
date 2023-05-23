fb64f7f166ccbbc9eb0e7771552a2b6236df5eb1
ye yint <yeyint@better.hr>
2023-05-23 18:16:38 +0630

    fixed

--- src/server.js
+++ src/server.js
@@ -2,8 +2,14 @@ import jsonServer from 'json-server';
 import { Low } from 'lowdb'
 import { JSONFile } from 'lowdb/node'
 
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // Configure lowdb to write data to JSON file
-const adapter = new JSONFile('./db.json')
+const adapter = new JSONFile(`${__dirname}/db.json`)
 const defaultData = { todos: [] }
 const db = new Low(adapter, defaultData)
 
